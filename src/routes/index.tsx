693bcafb38849b590984bf814f3fa05dc02967a8
Jesse Brault <jbrault@mac.com>
2024-08-07 11:35:02 -0500

    Working locally.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,30 @@
-import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { useRef } from 'react';
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { useRef } from 'react'
 
 export const Route = createFileRoute('/')({
-  component: () => {
-    const navigate = useNavigate();
+    component: () => {
+        const navigate = useNavigate()
 
-    const inputRef = useRef<HTMLInputElement>(null);
+        const inputRef = useRef<HTMLInputElement>(null)
 
-    const onQueryChange = () => {
-      if (inputRef.current !== null) {
-        navigate({ search: { query: inputRef.current.value } });
-      }
-    };
+        const onQueryChange = () => {
+            if (inputRef.current !== null) {
+                navigate({ search: { query: inputRef.current.value } })
+            }
+        }
 
-    return (
-      <div>
-        <p>Click the button.</p>
-        <input ref={inputRef} type="text" onChange={onQueryChange} />
-        <button onClick={() => navigate({ to: '/test' })}>Click me!</button>
-      </div>
-    );
-  },
-});
+        return (
+            <div>
+                <p>Enter a query and then click the button.</p>
+                <p>
+                    Notice that the query is not persisted through the
+                    navigation.
+                </p>
+                <input ref={inputRef} type="text" onChange={onQueryChange} />
+                <button onClick={() => navigate({ to: '/test' })}>
+                    Click me!
+                </button>
+            </div>
+        )
+    }
+})
--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute, useSearch } from '@tanstack/react-router';
+import { createFileRoute, useSearch } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/test')({
-  component: () => {
-    const search = useSearch({ from: '/test' });
-    return <div>Hello /test! The query is: {search.query}</div>;
-  },
-});
+    component: () => {
+        const search = useSearch({ from: '/test' })
+        return <div>Hello /test! The query is: {(search as any).query}</div>
+    }
+})
